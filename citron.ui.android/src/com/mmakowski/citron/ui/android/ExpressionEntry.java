70fb7850f5bc2887d98a158e84129b4f5f83ae43
Maciek Makowski <maciek.makowski@gmail.com>
2009-12-28 19:03:36 +0000

    initial commit

---
+++ citron.ui.android/src/com/mmakowski/citron/ui/android/ExpressionEntry.java
@@ -0,0 +1,13 @@
+package com.mmakowski.citron.ui.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExpressionEntry extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
